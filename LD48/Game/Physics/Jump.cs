bd554bd8c20504a644b2d0a5d8adef858ec6ff0f
Mazefest <colbymehmen@gmail.com>
2021-04-24 15:09:25 -0500

    Added very basic jump physics

--- LD48/Game/Physics/Jump.cs
+++ LD48/Game/Physics/Jump.cs
@@ -2,3 +2,52 @@
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using System.Diagnostics;
+
+namespace LD48
+{
+    public class JumpPhysics
+    {
+        public bool isActive;
+        public bool isFalling;
+
+        private int height;
+        private int maxJumpHeight;
+        private int heightIncrement;
+
+        public JumpPhysics()
+        {
+            height = 0;
+            maxJumpHeight = 90;
+            heightIncrement = 10;
+            isActive = false;
+            isFalling = false;
+        }
+
+        public void start()
+        {
+            isActive = true;
+            isFalling = false;
+        }
+
+        public void stop()
+        {
+            height = 0;
+            isActive = false;
+        }
+
+        public int getJumpY()
+        {
+            if (height < maxJumpHeight)
+            {
+                height += heightIncrement;
+                return heightIncrement;
+            }
+            else
+            {
+                stop();
+                isFalling = true;
+                return 0;
+            }
+        }
+    }
+}
