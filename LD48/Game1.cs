aa946515e87dc329eefcef46ffb8f61157357e83
wazawoo <reed.moseng@gmail.com>
2021-04-23 19:00:57 -0700

    Add tutorial content

--- LD48/Game1.cs
+++ LD48/Game1.cs
@@ -6,6 +6,10 @@ namespace LD48
 {
     public class Game1 : Game
     {
+        Texture2D ballTexture;
+        Vector2 ballPosition;
+        float ballSpeed;
+
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
@@ -19,6 +23,15 @@ namespace LD48
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+            _graphics.PreferredBackBufferWidth = 500;
+            _graphics.PreferredBackBufferWidth = 500;
+            _graphics.ApplyChanges();
+
+            ballPosition = new Vector2(
+                    _graphics.PreferredBackBufferWidth / 2,
+                    _graphics.PreferredBackBufferHeight / 2
+                );
+            ballSpeed = 100f;
 
             base.Initialize();
         }
@@ -28,6 +41,8 @@ namespace LD48
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+
+            ballTexture = Content.Load<Texture2D>("ball");
         }
 
         protected override void Update(GameTime gameTime)
@@ -36,15 +51,55 @@ namespace LD48
                 Exit();
 
             // TODO: Add your update logic here
+            var kstate = Keyboard.GetState();
+            var dt = (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            if (kstate.IsKeyDown(Keys.Up)) {
+                ballPosition.Y -= ballSpeed * dt;
+            }
+
+            if (kstate.IsKeyDown(Keys.Down)) {
+                ballPosition.Y += ballSpeed * dt;
+            }
+
+            if (kstate.IsKeyDown(Keys.Left))
+                ballPosition.X -= ballSpeed * dt;
+
+            if (kstate.IsKeyDown(Keys.Right))
+                ballPosition.X += ballSpeed * dt;
+
+            // restrict to bounds
+            if (ballPosition.X > _graphics.PreferredBackBufferWidth - ballTexture.Width / 2)
+                ballPosition.X = _graphics.PreferredBackBufferWidth - ballTexture.Width / 2;
+            else if (ballPosition.X < ballTexture.Width / 2)
+                ballPosition.X = ballTexture.Width / 2;
+
+            if (ballPosition.Y > _graphics.PreferredBackBufferHeight - ballTexture.Height / 2)
+                ballPosition.Y = _graphics.PreferredBackBufferHeight - ballTexture.Height / 2;
+            else if (ballPosition.Y < ballTexture.Height / 2)
+                ballPosition.Y = ballTexture.Height / 2;
 
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.DarkBlue);
 
             // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(
+                ballTexture,
+                ballPosition,
+                null,
+                Color.White,
+                0f,
+                new Vector2(ballTexture.Width / 2, ballTexture.Height / 2),
+                Vector2.One,
+                SpriteEffects.None,
+                0f
+            );
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
