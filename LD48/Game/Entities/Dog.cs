a33d86a96a43338a4d8d25cd164c296d39529e91
Mazefest <colbymehmen@gmail.com>
2021-04-24 20:28:10 -0500

    Added Dog Entity

---
+++ LD48/Game/Entities/Dog.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace LD48
+{
+    public class Dog : Entity
+    {
+
+        public Dog (
+            Vector2 position,
+            float speed)
+            : base(position, speed)
+        {
+//
+        }
+
+        public void Update(GameTime gameTime, Player owner) {
+            if (position.X < (owner.position.X - 10)) {
+                position.X += (float).1;
+            } else if (position.X > owner.position.X + 10)
+            {
+                position.X -= (float).1;
+            }
+        }    
+    }
+}
+
