c630a9c7ea220e3cb06636f52fc1d6ede26e555b
Mazefest <colbymehmen@gmail.com>
2021-04-27 17:29:58 -0500

    Added animation class

---
+++ LD48/Game/Animations/Animation.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace LD48.Game.Animations
+{
+    public class Animation
+    {
+        Texture2D texture;
+        float frameTime;
+        bool isLooping;
+
+        public Texture2D Texture
+        {
+            get { return texture; }
+        }
+
+        public float FrameTime
+        {
+            get { return frameTime;  }
+        }
+
+        public bool IsLooping
+        {
+            get { return isLooping; }
+        }
+
+        // FrameCount is currently configured to be
+        // calculated by width and height.
+        public int FrameCount
+        {
+            get { return Texture.Width / FrameHeight; }
+        }
+
+        public int FrameWidth
+        {
+            get { return Texture.Height; }
+        }
+
+        public int FrameHeight
+        {
+            get { return Texture.Height; }
+        }
+
+        // PARAM@texture:   Entire sprite sheet
+        // PARAM@frameTime: Time inbetween frames
+        // PARAM@isLooping: Continue to loop through sheet.. or nah
+        public Animation(Texture2D texture, float frameTime, bool isLooping)
+        {
+            this.texture = texture;
+            this.frameTime = frameTime;
+            this.isLooping = isLooping;
+        }
+    }
+}
