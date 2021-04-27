50e891203c66de2dc544eeb7a6285245aa3e24e0
Mazefest <colbymehmen@gmail.com>
2021-04-27 17:56:49 -0500

    Refactored animation player

--- LD48/Game/Animations/AnimationPlayer.cs
+++ LD48/Game/Animations/AnimationPlayer.cs
@@ -4,69 +4,67 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace LD48
 {
-    namespace Platformer2D
+
+    struct AnimationPlayer
     {
-        struct AnimationPlayer
+        Animation animation;
+        int frameIndex;
+        private float time;
+
+        public Animation Animation
         {
-            Animation animation;
-            int frameIndex;
-            private float time;
+            get { return animation; }
+        }
 
-            public Animation Animation
+        public int FrameIndex
+        {
+            get { return frameIndex; }
+        }
+
+        /// Bottom center of each frame.
+        public Vector2 Origin
+        {
+            get { return new Vector2(Animation.FrameWidth / 2.0f, Animation.FrameHeight); }
+        }
+
+        public void PlayAnimation(Animation animation)
+        {
+            // If already playing
+            if (Animation == animation) { return; }
+
+            // Start the new animation.
+            this.animation = animation;
+            this.frameIndex = 0;
+            this.time = 0.0f;
+        }
+
+        /// Advances the time position and draws the current frame of the animation.
+        public void Draw(GameTime gameTime, SpriteBatch spriteBatch, Vector2 position, SpriteEffects spriteEffects)
+        {
+            if (Animation == null) { throw new NotSupportedException("No animation is currently playing."); }
+
+            // Process passing time.
+            time += (float)gameTime.ElapsedGameTime.TotalSeconds;
+            while (time > Animation.FrameTime)
             {
-                get { return animation; }
-            }
+                time -= Animation.FrameTime;
 
-            public int FrameIndex
-            {
-                get { return frameIndex; }
-            }
-
-            /// Bottom center of each frame.
-            public Vector2 Origin
-            {
-                get { return new Vector2(Animation.FrameWidth / 2.0f, Animation.FrameHeight); }
-            }
-
-            public void PlayAnimation(Animation animation)
-            {
-                // If already playing
-                if (Animation == animation) { return; }
-
-                // Start the new animation.
-                this.animation = animation;
-                this.frameIndex = 0;
-                this.time = 0.0f;
-            }
-
-            /// Advances the time position and draws the current frame of the animation.
-            public void Draw(GameTime gameTime, SpriteBatch spriteBatch, Vector2 position, SpriteEffects spriteEffects)
-            {
-                if (Animation == null) { throw new NotSupportedException("No animation is currently playing."); }
-
-                // Process passing time.
-                time += (float)gameTime.ElapsedGameTime.TotalSeconds;
-                while (time > Animation.FrameTime)
+                // Advance the frame index; looping or clamping as appropriate.
+                if (Animation.IsLooping)
                 {
-                    time -= Animation.FrameTime;
-
-                    // Advance the frame index; looping or clamping as appropriate.
-                    if (Animation.IsLooping)
-                    {
-                        frameIndex = (frameIndex + 1) % Animation.FrameCount;
-                    }
-                    else
-                    {
-                        frameIndex = Math.Min(frameIndex + 1, Animation.FrameCount - 1);
-                    }
+                    frameIndex = (frameIndex + 1) % Animation.FrameCount;
                 }
-
-                // Calculate the source rectangle of the current frame.
-                Rectangle source = new Rectangle(FrameIndex * Animation.Texture.Height, 0, Animation.Texture.Height, Animation.Texture.Height);
-
-                // Draw the current frame.
-                spriteBatch.Draw(Animation.Texture, position, source, Color.White, 0.0f, Origin, 1.0f, spriteEffects, 0.0f);
+                else
+                {
+                    frameIndex = Math.Min(frameIndex + 1, Animation.FrameCount - 1);
+                }
             }
+
+            // Calculate the source rectangle of the current frame.
+            Rectangle source = new Rectangle(FrameIndex * Animation.Texture.Height, 0, Animation.Texture.Height, Animation.Texture.Height);
+
+            // Draw the current frame.
+            spriteBatch.Draw(Animation.Texture, position, source, Color.White, 0.0f, Origin, 1.0f, spriteEffects, 0.0f);
         }
     }
 }
